260bdbaa06f992c3a649d968bcb956a05560b70f
DeepNinja07x <rakshit.deepraj07@gmail.com>
2020-08-17 18:39:51 +0530

    Python-Scripts

---
+++ Basic Scripts/wifi_pass.py
@@ -0,0 +1,16 @@
+import subprocess
+data=subprocess.check_output(['netsh','wlan','show','profiels']).decode('utf-8',errors="backslashreplace").split('\n')
+profiles=[i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
+for i in profilles:
+    try:
+        results=subprocess.check_output(['netsh','wlan','show','profile',i,'key=clear']).decode('utf-8',errors="backslashreplace").split('\n')
+        results=[b.split(":")[1][1:-1] for b in results if "Key Content" in b]
+        try:
+            print("{:<30}| {:<}".format(i,results[0]))
+        except IndexError:
+            print("{:<30}| {:<}".format(i,""))
+    except subprocess.CalledProcessError:
+        print("{:<30}| {:<}".format(i,"ENCODING ERROR"))
+
+input("")
+        
