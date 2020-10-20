5e058af17457c1633d24a9da85b23f8efb8dbebc
Pedro <pedro1silva1vargas@gmail.com>
2020-10-20 09:49:36 -0300

    add number to hex converter

---
+++ Basic Scripts/number_to_hex.py
@@ -0,0 +1,16 @@
+hex_numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
+def number_to_hex(number_to_convert: int):
+    result = number_to_convert
+    hexadecimal = ""
+    while result != 0:
+        remainder = hex_numbers[result % 16]
+        hexadecimal = str(remainder) + hexadecimal
+        result = int(result / 16)
+    print(hexadecimal)
+
+if __name__ == "__main__":
+    """
+    >>> number_to_hex(7562)
+    1D8A
+    """
+    number_to_hex(7562)
