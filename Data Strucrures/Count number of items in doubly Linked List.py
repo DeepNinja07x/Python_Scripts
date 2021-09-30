1ab1b3760d9bb1ecb784490315446fe0964d19e4
Chandan Kumar Saha <70543351+chandankumar1307@users.noreply.github.com>
2021-10-01 00:51:56 +0530

    Added a Linked List Program
    Write a Python program to count the number of items of a given doubly linked list.

---
+++ Data Strucrures/Count number of items in doubly Linked List.py
@@ -0,0 +1,36 @@
+class Node(object):
+    # Singly linked node
+    def __init__(self, data=None, next=None, prev=None):
+        self.data = data
+        self.next = next
+        self.prev = prev
+
+class doubly_linked_list(object):
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.count = 0
+
+    def append_item(self, data):
+        # Append an item 
+        new_item = Node(data, None, None)
+        if self.head is None:
+            self.head = new_item
+            self.tail = self.head
+        else:
+            new_item.prev = self.tail
+            self.tail.next = new_item
+            self.tail = new_item
+
+        self.count += 1
+
+
+items = doubly_linked_list()
+items.append_item('PHP')
+items.append_item('Python')
+items.append_item('C#')
+items.append_item('C++')
+items.append_item('Java')
+items.append_item('SQL')
+
+print("Number of items of the  Doubly linked list:",items.count)
